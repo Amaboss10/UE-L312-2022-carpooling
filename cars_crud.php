c67bfe7655ee6a02ab22c35ba4113e4af2820a07
Gleen Camille <darsycamille2@gmail.com>
2022-11-25 01:52:59 +0100

    main: crud

---
+++ cars_crud.php
@@ -0,0 +1,227 @@
+<?php
+include_once 'index/header.php';
+
+use App\Controllers\CarsController;
+use App\Services\CarsService;
+
+require __DIR__ . '/vendor/autoload.php';
+$controller = new CarsController();
+$carsService = new CarsService();
+$cars = $carsService->getCars();
+echo $controller->getCars();
+?>
+<br>
+<table class="form">
+    <tr>
+        <td>
+            <form method="post" action="cars_crud.php" name ="carCreateForm">
+                <table class="form-control" id="tb_1">
+                    <tr>
+                        <td>
+                            <h5>Création d'une voiture</h5>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="idcar">Identifiant :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="idcar">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="numberplate">Plaque d'immatriculation :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="numberplate">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="brand">Marque :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="brand">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="model">Modèle :</label>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="model">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="color">Couleur :</label>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="color">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="year">Années :</label>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="year">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <br>
+                                <input type="submit" class="button" value="Créer">
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php echo $controller->createCar();?>
+                        </td>
+                    </tr>          
+                </table>
+            </form>
+        </td>
+
+        <td>
+            <form method="post" action="cars_crud.php" name ="carUpdateForm">
+                <table class="form-control" id='in-3'>
+                    <tr>
+                        <td>
+                            <h5>Mise à jour d'une voiture</h5>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="idup">Voiture :</label>
+                        </td>
+                        <td>
+                            <select name="idup" class="form-control" id="car-select">
+                                <option value="" selected disabled>Choisir une voiture</option>
+                                    <?php foreach ($cars as $car): ?>
+                                        <?php $carName = $car->getId().'' . $car->getNumberplate() .'' .$car->getBrand() . ' ' . $car->getModel() . ' - ' . $car->getType().'-'. $car->getColor() . $car->getYear(); ?>
+                                        <option  value="<?php echo $car->getId(); ?>"><?php echo $carName; ?></option>
+                                    <?php endforeach; ?>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="idcar">Identifiant :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="idcar">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="numberplate">Plaque d'immatriculation :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="numberplate">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="brand">Marque :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="brand">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="model">Modèle :</label>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="model">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="color">Couleur :</label>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="color">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="year">Années :</label>
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="year">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <br>
+                                <input type="submit" class="button" value="Mise à jour">
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php echo $controller->updateCar();?>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </td>
+        <td>
+
+
+            <form method="post" action="cars_crud.php" name ="carDeleteForm">
+                <table class="form-control" id='tb_2'>
+                    <tr>
+                        <td>
+                            <h5>Supression d'une voiture</h5>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="iddel">Voiture :</label>
+                        </td>
+                        <td>
+                            <select name="iddel" class="form-control" id="car-select">
+                                <option value="" selected disabled>Choisir une voiture</option>
+                                <?php foreach ($cars as $car): ?>
+                                        <?php $carName = $car->getId().'' . $car->getNumberplate() .'' .$car->getBrand() . ' ' . $car->getModel() . ' - ' . $car->getType().'-'. $car->getColor() . $car->getYear(); ?>
+                                        <option  value="<?php echo $car->getId(); ?>"><?php echo $carName; ?></option>
+                                    <?php endforeach; ?>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <br>
+                                <input type="submit" class="button" value="Supprimer">
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php echo $controller->deleteCar();?>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </td>
+    </tr>
+</table>
---
+++ reservations_crud.php
@@ -0,0 +1,231 @@
+<?php
+include_once'index/header.php';
+
+use App\Controllers\ReservationsController;
+use App\Services\ReservationsService;
+
+require __DIR__ . '/vendor/autoload.php';
+include_once 'index/header.php';
+$controller = new ReservationsController();
+echo $controller->getReservation();
+
+$reservationsService = new ReservationsService();
+$reservations = $reservationsService->getReservations();
+?>
+<br>
+<table class="form">
+    <tr>
+        <td>
+            <form method="post" action="reservations_crud.php" name ="reservationCreateForm">
+                <table class="form-control" id="in_1">
+                    <tr>
+                        <td class="title" >
+                            <h5>Création d'une réservation</h5>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="idreservation">Identifiant :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="idreservation">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="date">Date de départ :</label>  
+                        </td>
+                        <td>
+                        <input type="date" class="form-control" name="date"
+                        value="2022-11-25"
+                        min="2018-01-01" max="2024-12-31">
+                        </td>                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="departure_time">Heure de départ :</label>  
+                        </td>
+                        <td>
+                            <input type="time" class="form-control" name="departure_time"
+                            min="00:00" max="24:00" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="arriving_time">Heure d'arrivée :</label>  
+                        </td>
+                        <td>
+                            <input type="time" class="form-control" name="arriving_time"
+                            min="00:00" max="24:00" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="place_of_departure">Lieu de départ :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="place_of_departure">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="arrival_point">Point d'arrivée :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="arrival_point">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <br>
+                                <input type="submit" class="button" value="Créer">
+                            <br>
+                        </td>
+                    </tr> 
+                    
+                    <tr>
+                        <td>
+                            <?php echo $controller->createReservation();?>
+                        </td>
+                    </tr>          
+                </table>
+            </form>
+        </td>
+
+        <td>
+            <form method="post" action="reservations_crud.php" name ="reservationUpdateForm">
+                <table class="form-control" id='in-3'>
+                    <tr>
+                        <td>
+                            <h5>Mise à jour d'une réservation</h5>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="tbid">Réservation :</label>
+                        </td>
+                        <td>
+                            <select name="tbid" class="form-control" id="reservation-select">
+                                <option value="" selected disabled>Choisir une réservation</option>
+                                <?php foreach ($reservations as $reservation): ?>
+                                        <?php $reservationName ='#'. $reservation->getId(). ' a reservé pour le' . $reservation->getDate() . 'pour aller de ' . $reservation->getPlace_of_departure(). "à". $reservation->getArrival_point().'heure de départ'. $reservation->getDeparture_time(). "heure d'arrivée". $reservation->setArriving_time(); ?>
+                                        <!--<input type="checkbox" name="reservations[]" value="<?php //echo $reservation->getId(); ?>"><?php //echo $reservationName; ?>-->
+                                        <option  value="<?php echo $reservation->getId(); ?>"><?php echo $reservationName; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="idreservation">Identifiant :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="idreservation">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="date">Date de départ :</label>  
+                        </td>
+                        <td>
+                        <input type="date" class="form-control" name="date"
+                        value="2022-11-25"
+                        min="2018-01-01" max="2024-12-31">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="departure_time">Heure de départ :</label>  
+                        </td>
+                        <td>
+                            <input type="time" class="form-control" name="departure_time"
+                            min="00:00" max="24:00" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="arriving_time">Heure d'arrivée :</label>  
+                        </td>
+                        <td>
+                            <input type="time" class="form-control" name="arriving_time"
+                            min="00:00" max="24:00" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="place_of_departure">Lieu de départ :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="place_of_departure">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="arrival_point">Point d'arrivée :</label>  
+                        </td>
+                        <td>
+                            <input type="text" class="form-control" name="arrival_point">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <br>
+                                <input type="submit" class="button" value="Mise à jour">
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php echo $controller->updateReservation(); ?>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </td>
+
+        <td>
+            <form method="post" action="reservations_crud.php" name ="reservationDeleteForm">
+                <table class="form-control" id='in_2'>
+                    <tr>
+                        <td>
+                            <h5>Supression d'une réservation</h5>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <label for="id">Réservation :</label>
+                        </td>
+                        <td>
+                            <select name="iddel" class="form-control" id="reservation-select">
+                                <option value="" selected disabled>Choisir une réservation</option>
+                                <?php foreach ($reservations as $reservation): ?>
+                                        <?php $reservationName ='#'. $reservation->getId(). ' a reservé pour le' . $reservation->getDate() . 'pour aller de ' . $reservation->getPlace_of_departure(). "à". $reservation->getArrival_point().'heure de départ'. $reservation->getDeparture_time(). "heure d'arrivée". $reservation->setArriving_time(); ?>
+                                        <!--<input type="checkbox" name="reservations[]" value="<?php //echo $reservation->getId(); ?>"><?php //echo $reservationName; ?>-->
+                                        <option  value="<?php echo $reservation->getId(); ?>"><?php echo $reservationName; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <br>
+                                <input type="submit" class="button" value="Supprimer">
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <?php echo $controller->deleteReservation();?>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </td>
+    </tr>
+</table>
