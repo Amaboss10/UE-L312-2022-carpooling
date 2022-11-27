7c40eeb981d3c04cb6bf70e80f1b573aa0d44002
Gleen Camille <darsycamille2@gmail.com>
2022-11-27 22:41:46 +0100

    main : header and footer

--- index/header.php
+++ index/header.php
@@ -1,6 +1,5 @@
 <head>
     <meta charset="utf-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet" type="text/css">
     <title>Projet de Groupe Carpooling</title>
 </head>
@@ -12,7 +11,7 @@
                <li> <a class="nav-item nav-link" href="users_crud.php">Utilisateurs</a></li>
                <li> <a class="nav-item nav-link" href="cars_crud.php">Voitures</a></li>
                <li> <a class="nav-item nav-link" href="reservations_crud.php">Réservations</a></li>
-                <li><a class="nav-item nav-link" href="Post_crud.php">Annonces</a></li>
+                <li><a class="nav-item nav-link" href="post_crud.php">Annonces</a></li>
             </ul>
         </nav>
     </header>
